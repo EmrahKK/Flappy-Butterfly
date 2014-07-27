cbc41a4ca2808bc542efb2e026051bfe489098c3
EmrahKK <emrahkk@gmail.com>
2014-07-27 13:38:45 +0300

    Initial Commit

---
+++ Flappy Butterfly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flappy Butterfly
+//
+//  Created by Emrah Küçükkaya on 27/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flappy ButterflyTests/Flappy_ButterflyTests.m
@@ -0,0 +1,34 @@
+//
+//  Flappy_ButterflyTests.m
+//  Flappy ButterflyTests
+//
+//  Created by Emrah Küçükkaya on 27/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flappy_ButterflyTests : XCTestCase
+
+@end
+
+@implementation Flappy_ButterflyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
